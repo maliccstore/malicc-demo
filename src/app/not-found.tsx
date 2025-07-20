24d898d525474998611481b80572f7f7b0840202
Shahzer Baig <shahzer.baig@gmail.com>
2025-07-21 01:32:19 +0530

    Added(Custom 404 page)

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+// pages/404.js
+export default function Custom404() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you're looking for doesn't exist.</p>
+      <a href="/">Return to Home</a>
+    </div>
+  );
+}
