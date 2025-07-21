4a76209912c9a55e86a3c4766331e44c5441c064
Shahzer Baig <shahzer.baig@gmail.com>
2025-07-22 05:25:31 +0530

    Added(redux)

---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import appReducer from './slices/appSlice';
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      app: appReducer,
+      // Add other reducers here
+    },
+  });
+};
+
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>;
+// Infer the RootState and AppDispatch types
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
