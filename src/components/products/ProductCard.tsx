56823b35f1355ebfddca3a497d94248272127255
Shahzer Baig <shahzer.baig@gmail.com>
2025-07-22 05:35:36 +0530

    Removed(library: zustland)

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -2,11 +2,7 @@
 import Image from 'next/image';
 import { Button } from '../ui/button';
 import { Product } from '@/types/product';
-import { useCartStore } from '@/store/slices/cartStore';
-
 export default function ProductCard({ product }: { product: Product }) {
-  const addToCart = useCartStore((state) => state.addItem);
-
   return (
     <div className="w-64 border rounded-lg overflow-hidden">
       <Image
@@ -19,7 +15,7 @@ export default function ProductCard({ product }: { product: Product }) {
       <div className="p-4">
         <h3 className="font-bold">{product.name}</h3>
         <p className="text-gray-600">${product.price}</p>
-        <Button onClick={() => addToCart(product)}></Button>
+        <Button onClick={() => console.log(product)}></Button>
       </div>
     </div>
   );
--- src/store/slices/cartStore.ts
+++ src/store/slices/cartStore.ts
@@ -1,19 +1,19 @@
-import { Product } from '@/types/product';
-import { create } from 'zustand';
+// import { Product } from '@/types/product';
+// import { create } from 'zustand';
 
-interface CartState {
-  items: Product[];
-  addItem: (product: Product) => void;
-  removeItem: (productID: string) => void;
-  clearCart: () => void;
-}
+// interface CartState {
+//   items: Product[];
+//   addItem: (product: Product) => void;
+//   removeItem: (productID: string) => void;
+//   clearCart: () => void;
+// }
 
-export const useCartStore = create<CartState>((set) => ({
-  items: [],
-  addItem: (product) => set((state) => ({ items: [...state.items, product] })),
-  removeItem: (productID) =>
-    set((state) => ({
-      items: state.items.filter((item) => item.id !== productID),
-    })),
-  clearCart: () => set({ items: [] }),
-}));
+// export const useCartStore = create<CartState>((set) => ({
+//   items: [],
+//   addItem: (product) => set((state) => ({ items: [...state.items, product] })),
+//   removeItem: (productID) =>
+//     set((state) => ({
+//       items: state.items.filter((item) => item.id !== productID),
+//     })),
+//   clearCart: () => set({ items: [] }),
+// }));
