221ddda8b86355f5c46dfcfbb7c47563e08ae943
Shahzer Baig <shahzer.baig@gmail.com>
2025-08-07 14:41:58 +0530

    Updated(not-found.tsx): Added layout to the not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link';
+import { Container, Text } from '@radix-ui/themes';
+
 // pages/404.js
 export default function Custom404() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you're looking for doesn't exist.</p>
-      <a href="/">Return to Home</a>
-    </div>
+    <Container style={{ textAlign: 'center', marginTop: '50px' }}>
+      <Text>404 - Page Not Found</Text>
+      <Text>
+        &quot;Sorry, the page you&apos;re looking for doesn&apos;t exist.
+      </Text>
+      <Link href="/">Return to Home</Link>
+    </Container>
   );
 }
